83634203531e9ee7d385c778e1cd2d55e1b26037
pranayIntrigue <pranay@intrigueme.ca>
2020-03-31 18:03:21 -0400

    Added the dice game to randomly assign face images and changing state of the app

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 
-# Dicee 🎲
+# Dice 🎲
 
 ##  Goal
 
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
@@ -6,7 +7,7 @@ void main() {
       home: Scaffold(
         backgroundColor: Colors.red,
         appBar: AppBar(
-          title: Text('Dicee'),
+          title: Text('Dice'),
           backgroundColor: Colors.red,
         ),
         body: DicePage(),
@@ -15,9 +16,53 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDice = 1;
+  int rightDice = 1;
+
+  void changingFaces() {
+    leftDice = Random().nextInt(6) + 1;
+    rightDice = Random().nextInt(6) + 1;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: FlatButton(
+              onPressed: () {
+                //UPDATE STATE
+                setState(() {
+                  changingFaces();
+                });
+              },
+              child: Image(
+                image: AssetImage('images/dice$leftDice.png'),
+              ),
+            ),
+          ),
+          Expanded(
+            child: FlatButton(
+              onPressed: () {
+                //UPDATE STATE
+                setState(() {
+                  changingFaces();
+                });
+              },
+              child: Image.asset(
+                'images/dice$rightDice.png',
+              ),
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
